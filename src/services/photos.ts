aef5cf62ff685c2018a5e41b762fb8c8af549a6c
Sergii Taran <serg.taran1970@gmail.com>
2026-03-18 14:49:37 +0200

    feat: implement fetchPhotos service

--- src/services/photos.ts
+++ src/services/photos.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import type { Photo } from '../types/photo';
 
 axios.defaults.baseURL = 'https://api.pexels.com/v1/';
 axios.defaults.headers.common['Authorization'] =
@@ -6,4 +7,19 @@ axios.defaults.headers.common['Authorization'] =
 
 axios.defaults.params = {
   orientation: 'landscape',
+};
+
+interface PexelsResponse {
+  photos: Photo[];
+}
+
+export const fetchPhotos = async (query: string): Promise<Photo[]> => {
+  const res = await axios.get<PexelsResponse>('search', {
+    params: {
+      query,
+      per_page: 12,
+    },
+  });
+
+  return res.data.photos;
 };
